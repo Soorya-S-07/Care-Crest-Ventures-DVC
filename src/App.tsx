b7c8ed40a184578c496c5948821ed2e5af2e92d8
Soorya S <work.soorya@gmail.com>
2026-06-09 12:31:14 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="w-full">
+      <iframe
+        src="/visiting-card.html"
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+          display: 'block'
+        }}
+        title="Care Crest Ventures Digital Visiting Card"
+      />
+    </div>
+  );
+}
+
+export default App;
